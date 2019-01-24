59a97ffc5673af8871bafc2d4c330fe0e7bf825c
Pablo Alcalde <pablo.alcalde@cabify.com>
2019-01-24 16:35:10 +0100

    change names

--- KataLogInLogOut/KataLogInLogOut/ViewController.swift
+++ KataLogInLogOut/KataLogInLogOut/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func didEndEditing(_ sender: Any) {
+        switch sender {
+        case loginTextField as UITextField:
+            if loginTextField.text != "admin" {
+                fatalError("error!")
+            }
 
+        case passwordTextField as UITextField:
+            if passwordTextField.text != "admin" {
+                fatalError("error!")
+            }
+        default:
+            fatalError("unknown sender")
+        }
+    }
+
+    @IBAction func didTapOnLogIn(_ sender: Any) {
+        view.resignFirstResponder()
+    }
+
+    
 }
 
