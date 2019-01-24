8ef4800bd4460561f444fe565c24e2f70c7776ed
Pablo Alcalde <pablo.alcalde@cabify.com>
2019-01-24 15:50:45 +0100

    First commit

---
+++ KataLogInLogOut/KataLogInLogOut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KataLogInLogOut
+//
+//  Created by Pablo A on 24/01/2019.
+//  Copyright © 2019 Cabify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
