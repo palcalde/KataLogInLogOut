62aa0c1f17f002a2330f9f92c0e34e77d9cdb284
Pablo Alcalde <pablo.alcalde@cabify.com>
2019-01-24 16:52:35 +0100

    Finish first implementation

--- KataLogInLogOut/KataLogInLogOut/ViewController.swift
+++ KataLogInLogOut/KataLogInLogOut/ViewController.swift
@@ -12,17 +12,33 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
 
+    private var loggedIn = false {
+        didSet {
+            loginTextField.isHidden = loggedIn
+            passwordTextField.isHidden = loggedIn
+
+            if loggedIn {
+                logInButton.setTitle("Log Out", for: UIControl.State.normal)
+            } else {
+                logInButton.setTitle("Log In", for: UIControl.State.normal)
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func didEndEditing(_ sender: Any) {
     }
 
     @IBAction func didTapOnLogIn(_ sender: Any) {
+        loggedIn ? logOut() : logIn()
+    }
+
+    private func logIn() {
         if loginTextField.text != "admin" {
             fatalError("error!")
         }
@@ -31,7 +47,13 @@ class ViewController: UIViewController {
             fatalError("error!")
         }
 
+        loggedIn = true
+
         view.resignFirstResponder()
+    }
+
+    private func logOut() {
+        loggedIn = !(Int(Date.timeIntervalSinceReferenceDate) % 2 == 0)
     }
 
     
